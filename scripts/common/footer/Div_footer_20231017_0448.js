7145a13548f2992d05b5543b190ad7e5373abe88
Jaeseong Yoo <praster1@gmail.com>
2023-10-17 04:48:59 +0900

    footer에 페이스북 그룹 로고 추가

---
+++ scripts/common/footer/Div_footer_20231017_0448.js
@@ -0,0 +1,17 @@
+function Div_footer() {
+	return (
+		<div class="w-full">
+			<div class="flex flex-col w-full justify-center items-center text-center space-y-2">
+				<span class="text-sm">
+					Copyright © 2023 Korean International Statistical Society. All rights reserved.
+				</span>
+
+				<div class="flex flex-row">
+					<a href="https://www.facebook.com/groups/190717430950968" target="_blank">
+						<img src="https://cdn.jsdelivr.net/gh/statground/statkiss_CDN/images/svg/footer_facebook.svg" class="w-4 h-4" />
+					</a>
+				</div>
+			</div>
+		</div>
+	)
+}
