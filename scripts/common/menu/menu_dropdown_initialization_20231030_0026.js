85cab2b5f673a375b80a15eb0a3bfd2998f2a0d0
Jaeseong Yoo <praster1@gmail.com>
2023-10-30 00:28:00 +0900

    메뉴 수정

---
+++ scripts/common/menu/menu_dropdown_initialization_20231030_0026.js
@@ -0,0 +1,15 @@
+// 메뉴 하위메뉴 모두 닫기
+function menu_dropdown_initialization() {
+	document.getElementById("mega_menu_dropdown_about").className = "hidden"
+	toggle_menu_dropdown_about = 0
+	document.getElementById("mega_menu_dropdown_announcement").className = "hidden"
+	toggle_menu_dropdown_announcement = 0
+	document.getElementById("mega_menu_dropdown_publications").className = "hidden"
+	toggle_menu_dropdown_publications = 0
+	document.getElementById("mega_menu_dropdown_awards").className = "hidden"
+	toggle_menu_dropdown_awards = 0
+	document.getElementById("mega_menu_dropdown_forums").className = "hidden"
+	toggle_menu_dropdown_forums = 0
+	document.getElementById("mega_menu_dropdown_membership").className = "hidden"
+	toggle_menu_dropdown_membership = 0
+}
