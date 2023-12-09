f2ebd05f9bf2143aa9a7d368e4c4eefe7d42d5c5
Jaeseong Yoo <praster1@gmail.com>
2023-12-10 02:15:21 +0900

    Div_main

---
+++ scripts/index/Div_main.js
@@ -0,0 +1,37 @@
+function Div_main() {
+	return (
+		<div class="flex flex-col justify-center items-center w-full pt-12 pb-12 justify-center space-y-16">
+			<div id="div_main_latest_news" class="w-full"></div>
+			
+			<div class="grid grid-cols-4 w-full px-32 md:grid-cols-1 md:px-0 md:space-y-4">
+
+				<div class="col-span-3 flex flex-row justify-start items-start md:flex-col">
+					<div id="div_main_tab" class="flex flex-row w-fit md:justify-center md:items-center md:w-full">
+						<Div_main_tab />
+					</div>
+
+					<div id="div_main_tab_contents" class="md:w-full">
+						<div id="div_tab_kiss_event" class="md:w-full">
+							<div class="grid grid-cols-2 md:gap-8 md:space-y-0 border border-gray-300 p-8 w-full md:grid-cols-1"></div>
+						</div>
+						<div id="div_tab_advertisement" class="hidden">
+							<div class="grid grid-cols-2 md:gap-8 md:space-y-0 border border-gray-300 p-8 w-full md:grid-cols-1"></div>
+						</div>
+						<div id="div_tab_member_news" class="hidden">
+							<div class="grid grid-cols-2 md:gap-8 md:space-y-0 border border-gray-300 p-8 w-full md:grid-cols-1"></div>
+						</div>
+						<div id="div_tab_job_related_news" class="hidden">
+							<div class="grid grid-cols-2 md:gap-8 md:space-y-0 border border-gray-300 p-8 w-full md:grid-cols-1"></div>
+						</div>
+					</div>
+				</div>
+
+				<div id="div_main_tab_banner"></div>
+			</div>
+
+
+			<div id="div_main_footer_banner" class="w-full"></div>
+			
+		</div>
+	)
+}
