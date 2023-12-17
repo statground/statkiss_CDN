bb7189c5d84043d2b06919de83c4b9dae7ccc841
Jaeseong Yoo <praster1@gmail.com>
2023-12-18 04:46:54 +0900

    header 메뉴

---
+++ scripts/common/menu/Div_sub_header.js
@@ -0,0 +1,6 @@
+// 헤더
+function Div_sub_header() {
+	return (
+		<div onclick="menu_dropdown_initialization();" id="div_menu_sub_header" class="w-full"></div>
+	)
+}
---
+++ scripts/common/menu/get_user_role.js
@@ -0,0 +1,68 @@
+async function get_user_role() {
+	function Div_sub_menu_header() {
+		return (
+			<div onclick="menu_dropdown_initialization();" id="div_menu_sub_header"
+			class="flex bg-blue-200 justify-center items-center w-full h-[35px]">
+			{   gv_username == ""
+				?   <div class="flex justify-end items-center text-end text-sm space-x-4 w-full h-full px-[35px]">
+						<a href="/account/" class="hover:underline">
+							Login
+						</a>
+						<span>
+							|
+						</span>
+						<a href="/account/signup/" class="hover:underline">
+							Sign Up
+						</a>
+					</div>
+				:   ""
+			}
+			{   gv_username != "" && gv_role == "Administrator"
+				?   <div class="flex justify-end items-center text-end text-sm space-x-4 w-full h-full px-[35px]">
+						<a href="/admin/" class="hover:underline">
+							Admin Page
+						</a>
+						<span>
+							|
+						</span>
+						<a href="/account/myinfo/" class="hover:underline">
+							My Info.
+						</a>
+						<span>
+							|
+						</span>
+						<a href="/account/logout/" class="hover:underline">
+							Logout
+						</a>
+					</div>
+				:   ""
+			}
+			{   gv_username != "" && gv_role != "Administrator"
+				?   <div class="flex justify-end items-center text-end text-sm space-x-4 w-full h-full px-[35px]">
+						<a href="/account/myinfo/" class="hover:underline">
+							My Info.
+						</a>
+						<span>
+							|
+						</span>
+						<a href="/account/logout/" class="hover:underline">
+							Logout
+						</a>
+					</div>
+				:   ""
+			}
+		</div>
+
+		)
+	}
+
+	const data = await fetch("/ajax_get_user_role/")
+					.then(res=> { return res.json(); })
+					.then(res=> { return res; });
+
+	gv_role = data['name']
+	console.log("*** role: " + gv_role);
+
+	// Menu
+	ReactDOM.render(<Div_sub_menu_header />, document.getElementById("div_menu_sub_header")) 
+}
