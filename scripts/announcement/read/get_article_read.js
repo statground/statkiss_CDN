5488bd79a6825ce4714ebd9ab4ff47254766606c
Jaeseong Yoo <praster1@gmail.com>
2023-12-25 19:09:18 +0900

    get_article_read

---
+++ scripts/announcement/read/get_article_read.js
@@ -0,0 +1,33 @@
+async function get_article_read() {
+	function Div_title(props) {
+		return (
+			<div class="flex flex-col w-full">
+				<h2 class="flex items-center mb-4 text-xl font-bold leading-none text-gray-900 md:text-2xl">
+					{props.title}
+				</h2>
+				
+				<dl class="flex items-center mb-4 space-x-4 md:mb-5">
+					<div>
+						<dd class="flex items-center text-gray-900">
+							<span class="font-semibold">{props.created_at}</span>
+						</dd>
+					</div>
+				</dl>
+			</div>
+		)
+	}
+
+	const request_data = new FormData();
+	request_data.append('uuid', uuid);
+	
+	const data = await fetch("/announcement/ajax_get_article_read/", {
+						method: "post", 
+						headers: { "X-CSRFToken": getCookie("csrftoken"), },
+						body: request_data
+						})
+						.then(res=> { return res.json(); })
+						.then(res=> { return res; });
+
+	ReactDOM.render(<Div_title title={data.title} created_at={data.created_at} />, document.getElementById("div_title"))
+	document.getElementById("div_contents").innerHTML = data.content
+}
