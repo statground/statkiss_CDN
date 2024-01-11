49f2bea58d30261738f3f7aa997a6f6b79b02f2d
Jaeseong Yoo <praster1@gmail.com>
2024-01-12 03:58:46 +0900

    uuid를 hidden variable로 변경

---
+++ scripts/account/myinfo/get_userinfo_20240112_0358.js
@@ -0,0 +1,18 @@
+async function get_userinfo() {
+	const data = await fetch("/account/ajax_get_userinfo/")
+					.then(res=> { return res.json(); })
+					.then(res=> { return res; });
+
+	document.getElementById("txt_email").value = data.email
+	document.getElementById("txt_name").value = data.name
+	document.getElementById("sel_gender").value = data.gender
+	document.getElementById("txt_affiliation").value = data.affiliation
+	document.getElementById("txt_title").value = data.title
+	document.getElementById("sel_education").value = data.education
+	document.getElementById("txt_interest").value = data.interest
+
+	uuid_user = data.uuid
+	uuid_role = data.uuid_role
+
+	input_checker()
+}
