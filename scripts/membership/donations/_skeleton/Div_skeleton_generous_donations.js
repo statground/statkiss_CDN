151aa24331705519025d29c027e31a20e57465a1
Jaeseong Yoo <praster1@gmail.com>
2024-01-18 03:44:50 +0900

    Div_skeleton_generous_donations

---
+++ scripts/membership/donations/_skeleton/Div_skeleton_generous_donations.js
@@ -0,0 +1,23 @@
+function Div_skeleton_generous_donations() {
+	return (
+		<div class="flex flex-col justify-center items-start py-8 px-4 mx-auto max-w-screen-xl space-y-8 animate-pulse">
+			<div class="max-w-screen-md mb-2">
+				<Div_sub_header text={"Our sincere appreciation for your generous donations to KISS!"} />
+			</div>
+		
+			<div class="flex flex-col w-full justify-center items-center">
+				<div class="h-2.5 bg-gray-200 rounded-full w-48 mb-4"></div>
+		
+				<div class="grid grid-cols-6 w-full sm:grid-cols-2 gap-4 md:grid-cols-4">
+					<div class="flex items-center justify-center w-full h-36 max-w-sm bg-gray-300 rounded-lg px-4"></div>
+					<div class="flex items-center justify-center w-full h-36 max-w-sm bg-gray-300 rounded-lg px-4"></div>
+					<div class="flex items-center justify-center w-full h-36 max-w-sm bg-gray-300 rounded-lg px-4"></div>
+					<div class="flex items-center justify-center w-full h-36 max-w-sm bg-gray-300 rounded-lg px-4"></div>
+					<div class="flex items-center justify-center w-full h-36 max-w-sm bg-gray-300 rounded-lg px-4"></div>
+					<div class="flex items-center justify-center w-full h-36 max-w-sm bg-gray-300 rounded-lg px-4"></div>
+				</div>
+			</div>
+
+		</div>
+	)
+}
