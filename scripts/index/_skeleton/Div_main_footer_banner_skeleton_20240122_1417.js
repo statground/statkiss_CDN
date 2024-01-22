10fdbae4d422ef279076dd75aa99bf76e2405a55
JaeseongYoo <praster1@gmail.com>
2024-01-22 14:18:29 +0900

    변수명 변경

---
+++ scripts/index/_skeleton/Div_main_footer_banner_skeleton_20240122_1417.js
@@ -0,0 +1,17 @@
+function Div_main_footer_banner_skeleton() {
+	return (
+		<div class="flex flex-nowrap justify-start items-center w-full space-x-4 px-32 overflow-x-scroll scroll-smooth scroll-hide">
+			<div class={class_tab_footer_banner_skeleton}></div>
+			<div class={class_tab_footer_banner_skeleton}></div>
+			<div class={class_tab_footer_banner_skeleton}></div>
+			<div class={class_tab_footer_banner_skeleton}></div>
+			<div class={class_tab_footer_banner_skeleton}></div>
+			<div class={class_tab_footer_banner_skeleton}></div>
+			<div class={class_tab_footer_banner_skeleton}></div>
+			<div class={class_tab_footer_banner_skeleton}></div>
+			<div class={class_tab_footer_banner_skeleton}></div>
+			<div class={class_tab_footer_banner_skeleton}></div>
+			<div class={class_tab_footer_banner_skeleton}></div>
+		</div>
+	)
+}
