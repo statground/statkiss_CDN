3508c7d74db2c20bfb2f8c41c8374fd42c964d23
Jaeseong Yoo <praster1@gmail.com>
2024-01-23 18:32:31 +0900

    skeleton 수정

---
+++ scripts/forums/_skeleton/Div_main_skeleton_20240123_1832.js
@@ -0,0 +1,22 @@
+function Div_main_skeleton() {
+	function Div_sub() {
+		return (
+			<a class="flex justify-between items-center p-4 mb-6 bg-gray-400 rounded-lg border-l-8 shadow hover:bg-gray-50 cursor-pointer">
+				<div>
+					<span class="block mb-1 text-xs font-medium text-gray-500 uppercase">　 　</span>
+					<span class="text-xl font-semibold text-purple-600">　 　</span>
+				</div>
+			</a>
+		)
+	}
+
+	return (
+		
+		<div class="mx-auto max-w-screen-xl sm:py-16 lg:px-6 animate-pulse">
+			<div id="div_buttons"></div>
+			<div id="div_links">
+				<Div_sub /><Div_sub /><Div_sub /><Div_sub /><Div_sub />
+			</div>
+		</div>
+	)
+}
