304d2e77705b06c78edec5fce19c30cd1776d950
Jaeseong Yoo <praster1@gmail.com>
2024-01-26 22:14:17 +0900

    click_btn_delete

---
+++ scripts/pubs/newsletter/click_btn_delete.js
@@ -0,0 +1,15 @@
+async function click_btn_delete(uuid) {
+	if(confirm("Are you sure you want to delete it?")){
+		// 인증 코드
+		const inputData = new FormData();
+		inputData.append('uuid', uuid);
+
+		data_user = await fetch("/forums/ajax_delete_links/", {
+								method: "post", 
+								headers: {"X-CSRFToken": getCookie("csrftoken"),},
+								body: inputData
+							})
+							.then(res=> { location.href="/pubs/newsletter/" })
+							.then(res=> { location.href="/pubs/newsletter/" });
+	}
+}
