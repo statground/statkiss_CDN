803a19186beac3bf4d1d498204b12fcdab28a6be
Jaeseong Yoo <praster1@gmail.com>
2024-01-26 15:14:31 +0900

    Div_main

---
+++ scripts/account/change_password/auth/Div_main_header.js
@@ -0,0 +1,7 @@
+function Div_main_header() {
+	return (
+		<div class="text-lg font-bold">
+			Change Password (2/2)
+		</div>
+	)
+}
---
+++ scripts/account/change_password/auth/check_auth_code.js
@@ -0,0 +1,19 @@
+async function check_auth_code() {
+	// 인증 코드
+	const inputData = new FormData();
+	inputData.append('auth_code', auth_code);
+
+	const data = await fetch("/account/ajax_check_auth_code/", {
+							method: "post", 
+							headers: {"X-CSRFToken": getCookie("csrftoken"),},
+							body: inputData
+						})
+						.then(res=> { return res.json(); })
+						.then(res=> { return res; });
+
+	if (data.status == "EXPIRED" || data.checker == "EXPIRED") {
+		ReactDOM.render(<Div_main_Error text={"Auth Code has expired."} />, document.getElementById("div_main"))
+	} else {
+		ReactDOM.render(<Div_main />, document.getElementById("div_main"))
+	}
+}
