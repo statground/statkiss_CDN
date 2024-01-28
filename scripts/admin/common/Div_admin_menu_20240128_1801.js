3b203e693c28305a7b545daf5e8a2d33f4b528cc
Jaeseong Yoo <praster1@gmail.com>
2024-01-28 18:01:32 +0900

    메뉴 버튼 작동 bug fix

---
+++ scripts/admin/common/Div_admin_menu_20240128_1801.js
@@ -0,0 +1,30 @@
+function Div_admin_menu() {
+	return (
+		<div class="flex flex-row justify-center items-center">
+			<button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 
+						group-hover:from-purple-600 group-hover:to-blue-500 
+						hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300"
+						onClick={() => location.href="/admin/summary/"}>
+				<span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-opacity-0">
+					Summary
+				</span>
+			</button>
+			<button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 
+						group-hover:from-pink-500 group-hover:to-orange-400 
+						hover:text-white focus:outline-none focus:ring-pink-200"
+						onClick={() => location.href="/admin/pageview/"}>
+				<span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+					Page View
+				</span>
+			</button>
+			<button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 
+						group-hover:from-pink-500 group-hover:to-orange-400 
+						hover:text-white focus:outline-none focus:ring-pink-200"
+						onClick={() => location.href="/admin/member/"}>
+				<span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+					Member
+				</span>
+			</button>
+		</div>
+	)
+}
