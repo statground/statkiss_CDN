6214c07568be0b77b3440b9499698df9c96dbdfa
Jaeseong Yoo <praster1@gmail.com>
2024-01-29 00:15:04 +0900

    관리자 여부 확인

---
+++ scripts/admin/common/set_main_2020129_0014.js
@@ -0,0 +1,28 @@
+async function set_main() {
+	function Div_main() {
+		return (
+			<div class="flex flex-col w-full max-w-screen-xl px-4 py-8 mx-auto lg:px-6 sm:py-16 lg:py-24 space-y-4">
+				<div class="w-full" id="div_admin_menu"></div>
+				<div class="flex flex-col justify-center items-center" id="div_content"></div>
+			</div>
+		)
+	}
+
+	// 스켈레톤
+	ReactDOM.render(<Div_main />, document.getElementById("div_main"))
+
+	const data = await fetch("/ajax_get_menu_header/")
+					.then(res=> { return res.json(); })
+					.then(res=> { return res; });
+
+	gv_role = data['name']
+	
+
+	if (gv_username == "" || (gv_role != "Administrator" && gv_role != "Officer")) {
+		location.href='/'
+	} else {
+		// 화면
+		ReactDOM.render(<Div_admin_menu />, document.getElementById("div_admin_menu"))
+		set_content()
+	}
+}
