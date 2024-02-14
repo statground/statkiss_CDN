7e452d9667bd0d0dd4590866100a5b325ea0822e
Jaeseong Yoo <praster1@gmail.com>
2024-02-15 02:04:59 +0900

    폴더 변경

--- scripts/functions/change_title.js
+++
@@ -1,7 +0,0 @@
-// Change Title
-function change_title(inputTitle) {
-	// 메타 태그 변경
-	inputTitle += " / Korean International Statistical Society"
-	document.title = inputTitle
-	$("meta[property='og\\:title']").attr("content", inputTitle);
-}
