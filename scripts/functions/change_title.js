b5cf0ac64e8f544a490c85db2589b9f0baff2d54
Jaeseong Yoo <praster1@gmail.com>
2024-02-15 01:41:18 +0900

    타이틀 변경

--- scripts/functions/change_title.js
+++ scripts/functions/change_title.js
@@ -1,7 +1,7 @@
 // Change Title
 function change_title(inputTitle) {
 	// 메타 태그 변경
-	inputTitle += " / 통계마당 Statistical Ground"
+	inputTitle += " / Korean International Statistical Society"
 	document.title = inputTitle
 	$("meta[property='og\\:title']").attr("content", inputTitle);
 }
