b4a334f647bee2b1aad5e26b2523356995b8c3a5
Jaeseong Yoo <praster1@gmail.com>
2024-02-17 00:46:15 +0900

    Div_member_count_skeleton

---
+++ scripts/admin/member/Div_member_count_skeleton.js
@@ -0,0 +1,13 @@
+function Div_member_count_skeleton(props) {
+	return (
+		<div class="flex flex-row w-full justify-between items-center">
+			<p>
+				<div class="h-2.5 bg-gray-200 rounded-full w-48 mb-4"></div>
+			</p>
+
+			<div>
+				<div class="h-2.5 bg-gray-200 rounded-full w-48 mb-4"></div>
+			</div>
+		</div>
+	)
+}
