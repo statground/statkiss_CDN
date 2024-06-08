2ab1f69d422e8005d832538b432f5b0117090cf6
Jaeseong Yoo <praster1@gmail.com>
2024-06-08 16:32:01 +0900

    get_div_add_newsletter_20240608_1631

---
+++ scripts/pubs/newsletter/get_div_add_newsletter_20240608_1631.js
@@ -0,0 +1,15 @@
+async function get_div_add_newsletter() {
+	const data = await fetch("/ajax_get_menu_header/")
+	.then(res=> { return res.json(); })
+	.then(res=> { return res; });
+
+	gv_role = data['name']
+
+	// Menu
+	if (gv_username != "" && (data.role == "Administrator" || data.role == "Developer" || data.officer != "Member")) {
+		ReactDOM.render(<Div_add_newsletter />, document.getElementById("div_add_newsletter"))
+		for (var i = 0 ; i < Object.keys(data_newsletter).length ; i++) {
+			document.getElementById("btn_delete_" + data_newsletter[Object.keys(data_newsletter)[i]].uuid).className = "flex flex-row justify-end items-end w-full"
+		}
+	}
+}
